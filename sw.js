727a7599e831467bb2d77eaac96ffea23073addb
juniorzhp <juniorzhp@sina.com>
2022-05-15 23:33:18 +0800

    Updates

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'weather-cache';
+
+const FILE_TO_CACHE = [
+  '/weather-gh-page'
+];
+
+this.addEventListener('install', function(event) {
+  this.skipWaiting();
+  console.log('install service worker.');
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILE_TO_CACHE);
+    })
+  );
+});
+
+this.addEventListener('active', function(event) {
+  console.log('service worker is active.')
+});
+
+this.addEventListener('fetch', function(event) {
+  const requestToCache = event.request.clone();
+  event.responseWith(
+    fetch(requestToCache).then(function(response) {
+      // 加载失败
+      if (!response || response.status !== 200) {
+        throw Error('response error');
+      }
+
+      // 加载成功，更新缓存
+      const responseToCache = response.clone();
+      caches.open(CACHE_NAME).then(function(cache) {
+        cache.put(requestToCache, responseToCache);
+      })
+
+      return response;
+    }).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        return response;
+      })
+    })
+  );
+});
